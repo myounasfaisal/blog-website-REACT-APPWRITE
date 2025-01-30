5ebfe91123d3fbe97fb0c9608f7222c1c008b937
myfAirCS <younasf69@gmail.com>
2025-01-30 18:46:41 +0500

    Created Necessary Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-  import './App.css'
+  import './App.css';
+  import { useState,useEffect } from 'react';
+  import {useDispatch} from 'react-redux';
+  import { login,logout } from './redux/authSlice/authSlice';
+import authService from './appwrite/auth';
 
   function App() {
+    const [isLoading,setIsLoading]=useState(true);
 
-    console.log(import.meta.env.VITE_APPWRITE_URL)
+    const dispatch=useDispatch();
 
-    return (
-      <>
-    <h1>Hello</h1>
-      </>
-    )
+    useEffect(()=>{
+      authService.getCurrentUser().then((userData)=>{
+        if(userData){
+          dispatch(()=>login(userData))
+        }else{
+          dispatch(()=>logout())
+        }
+      })
+      .finally(()=> setIsLoading(false));
+    },[])
+
+return isLoading ?  (
+  <div>is Loading ... </div>     
+) : ( <div>Testing</div>)
   }
 
   export default App
